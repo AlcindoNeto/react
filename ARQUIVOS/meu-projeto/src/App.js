b009546541386e6a2eb93b8f89e3a33b12911bfe
AlcindoNeto <102867139+AlcindoNeto@users.noreply.github.com>
2024-05-06 14:01:06 -0300

    criando o projeto e alterando JSX

---
+++ ARQUIVOS/meu-projeto/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = 'Alce'
+  const newName = name.toLocaleUpperCase()
+
+  function sum(a, b){
+    return a + b;
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
+  return (
+    <div className="App">
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha imagem"/>
+    </div>
+  );
+}
+
+export default App;
