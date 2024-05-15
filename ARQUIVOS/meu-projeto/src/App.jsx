eaa46f7592a1e9b692fe39039634dc5a2f62c905
AlcindoNeto <102867139+AlcindoNeto@users.noreply.github.com>
2024-05-15 11:10:31 -0300

    React routers

--- ARQUIVOS/meu-projeto/src/App.jsx
+++ ARQUIVOS/meu-projeto/src/App.jsx
@@ -11,11 +11,18 @@ import OutraLista from './components/OutraLista.jsx';
 import { useState } from 'react';
 import SeuNome from './components/SeuNome.jsx';
 import Saudacao from './components/Saudacao.jsx';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx';
+import Empresa from './pages/Empresa.jsx';
+import Contato from './pages/Contato.jsx';
+import Navbar from './components/layout/Navbar.jsx';
+import Footer from './components/layout/Footer.jsx';
+
 
 
 function App() {
-  const [nome, setNome] = useState()
-  const meusItens = ['React', 'Vue', 'Angular']
+ /*  const [nome, setNome] = useState()
+  const meusItens = ['React', 'Vue', 'Angular'] */
 /*   const name = 'Alce'
   const newName = name.toLocaleUpperCase()
   const nome = 'Joao'
@@ -27,8 +34,17 @@ function App() {
 
 
   return (
-    <div className="App">
-      {/* <h2>Alterando o JSX</h2>
+    <Router>
+      <Navbar/>
+      <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/empresa" element={<Empresa />} />
+      </Routes>
+      <Footer/>
+    </Router>
+    //<div className="App">
+      /* <h2>Alterando o JSX</h2>
       <p>Olá, {newName}</p>
       <p>Soma: {sum(1, 2)}</p>
       <Frase/>
@@ -41,21 +57,21 @@ function App() {
       profissao="programador"
       foto='https://via.placeholder.com/150'
       />
-      <List/> */}
-      {/* <Evento numero={1}/>
-      <Evento numero={2}/> */}
-      {/* <Form/>
-      <Evento/> */}
-      {/* <h1>Renderização Condicional</h1>
-      <Condicional/> */}
-      {/* <h1>Renderização de Listas</h1>
+      <List/> */
+      /* <Evento numero={1}/>
+      <Evento numero={2}/> */
+      /* <Form/>
+      <Evento/> */
+      /* <h1>Renderização Condicional</h1>
+      <Condicional/> */
+      /* <h1>Renderização de Listas</h1>
       <OutraLista itens={meusItens}/>
-      <OutraLista itens={[]}/> */}
-      <h1>State Lift</h1>
+      <OutraLista itens={[]}/> */
+      /* <h1>State Lift</h1>
       <SeuNome setNome={setNome}/>
-      <Saudacao nome={nome}/>
-    </div>
-  );
+      <Saudacao nome={nome}/> */
+    //</div>
+  )
 }
 
 export default App;
